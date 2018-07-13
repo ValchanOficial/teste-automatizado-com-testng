064449ee815b92c2c812ebfe9642d0d7f18c22ac
Valéria Padilha de Vargas <valeria_padilha@yahoo.com.br>
2018-07-13 14:48:19 -0300

    Seletor baseado em expressões xPath

--- src/test/java/br/com/treinaweb/selenium/tests/CadastroUsuarioTest.java
+++ src/test/java/br/com/treinaweb/selenium/tests/CadastroUsuarioTest.java
@@ -1,6 +1,7 @@
 package br.com.treinaweb.selenium.tests;
 
 import org.openqa.selenium.By;
+import org.openqa.selenium.support.ui.Select;
 import org.testng.Assert;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
@@ -25,5 +26,20 @@ public class CadastroUsuarioTest extends TreinaWebSeleniumTestBase{
 		getDriver().findElement(By.id("btn-salvar")).click();
 		Assert.assertTrue(getDriver().findElement(By.id("div-erro-cadastro-usuario")).isDisplayed());
 		Assert.assertTrue(getDriver().findElement(By.id("div-val-nome")).isDisplayed());
+		Assert.assertTrue(getDriver().findElement(By.id("txb-nome")).getAttribute("class").contains("is-invalid"));
+	}
+	
+	@Test(priority = 3)
+	public void verificarUsuarioPreenchidoTest() {
+		getDriver().findElement(By.id("txb-nome")).sendKeys("teste");
+		getDriver().findElement(By.id("txb-email")).sendKeys("teste@test.com");
+		getDriver().findElement(By.id("txb-confirmar-email")).sendKeys("teste@test.com");
+		getDriver().findElement(By.id("txb-senha")).sendKeys("senha123");
+		getDriver().findElement(By.id("txb-confirmar-senha")).sendKeys("senha123");
+		getDriver().findElement(By.cssSelector("label.custom-control:nth-child(3)")).click();
+		new Select(getDriver().findElement(By.id("sel-estado"))).selectByValue("SP");
+		getDriver().findElement(By.id("btn-salvar")).click();
+		Assert.assertTrue(getDriver().findElement(By.id("div-sucesso-cadastro-usuario")).isDisplayed());
+		Assert.assertEquals(getDriver().findElement(By.xpath("//table[@id='tbl-usuarios']/tbody/tr[1]/td[1]")).getText(), "teste");
 	}
 }
